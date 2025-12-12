5c0420efd736983416fb6dcb6e84fbc400c4ab96
Jakub Janicki <jjanicki08@gmail.com>
2025-12-12 19:20:19 +0100

    fixed tests

--- src/app/pages/dashboard/nutrition-tracker-page/nutrition-tracker-page.component.spec.ts
+++ src/app/pages/dashboard/nutrition-tracker-page/nutrition-tracker-page.component.spec.ts
@@ -1,6 +1,8 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
 
 import { NutritionTrackerPageComponent } from './nutrition-tracker-page.component';
+import { provideHttpClient } from '@angular/common/http';
+import { provideHttpClientTesting } from '@angular/common/http/testing';
 
 describe('NutritionTrackerPageComponent', () => {
   let component: NutritionTrackerPageComponent;
@@ -8,9 +10,9 @@ describe('NutritionTrackerPageComponent', () => {
 
   beforeEach(async () => {
     await TestBed.configureTestingModule({
-      imports: [NutritionTrackerPageComponent]
-    })
-    .compileComponents();
+      imports: [NutritionTrackerPageComponent],
+      providers: [provideHttpClient(), provideHttpClientTesting()],
+    }).compileComponents();
 
     fixture = TestBed.createComponent(NutritionTrackerPageComponent);
     component = fixture.componentInstance;
